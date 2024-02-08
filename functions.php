7437401b177f8ff487f7e2febfed99ccd9278639
Himanshu Pharswan <himanshupharswan67@gmail.com>
2024-02-08 18:03:30 +0530

    Edit functions.php

--- functions.php
+++ functions.php
@@ -144,3 +144,64 @@ function legalpress_setup() {
 }
 endif;
 add_action( 'after_setup_theme', 'legalpress_setup' );
+
+
+
+/**
+ * Register widget area.
+ *
+ * @link https://developer.wordpress.org/themes/functionality/sidebars/#registering-a-sidebar
+ */
+function legalpress_widgets_init() {
+	register_sidebar( array(
+		'name'          => esc_html__( 'Blog Sidebar', 'legalpress' ),
+		'id'            => 'sidebar-1',
+		'description'   => esc_html__( 'Add widgets here.', 'legalpress' ),
+		'before_widget' => '<div id="%1$s" class="widget %2$s">',
+		'after_widget'  => '</div>',
+		'before_title'  => '<h2 class="widget-title">',
+		'after_title'   => '</h2>',
+	) );
+
+	if(true===get_theme_mod( 'legalpress_enable_menu_left_sidebar',false)) :
+        register_sidebar( array(
+            'name'          => esc_html__( 'Menu Left Sidebar', 'legalpress' ),
+            'id'            => 'menuleftsidebar',
+            'description'   => esc_html__( 'Add widgets here.', 'legalpress' ),
+            'before_widget' => '<div id="%1$s" class="widget %2$s">',
+            'after_widget'  => '</div>',
+            'before_title'  => '<h4 class="widget-title">',
+			'after_title'   => '</h4>',
+        ) );
+    endif;
+
+}
+add_action( 'widgets_init', 'legalpress_widgets_init' );
+
+
+
+/**
+ * Display Dynamic CSS.
+ */
+function legalpress_dynamic_css_wrap() {
+	// require_once( get_parent_theme_file_path( '/css/dynamic.css.php' ) );  
+	?>
+  		<style type="text/css" id="legalpress-dynamic-style">
+    		<?php echo legalpress_dynamic_css_stylesheet(); ?>
+  		</style>
+	<?php 
+}
+add_action( 'wp_head', 'legalpress_dynamic_css_wrap' );
+
+
+
+/**
+ * Add a pingback url auto-discovery header for singularly identifiable articles.
+ */
+function legalpress_pingback_header() {
+	if ( is_singular() && pings_open() ) {
+	   printf( '<link rel="pingback" href="%s">' . "\n", get_bloginfo( 'pingback_url' ) );
+	 }
+}
+add_action( 'wp_head', 'legalpress_pingback_header' );
+
