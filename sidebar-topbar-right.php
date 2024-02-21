4751054f165cb2a1f462d04c1d9059f7b1e0f998
Himanshu Pharswan <himanshupharswan67@gmail.com>
2024-02-21 17:05:59 +0530

    New changes

---
+++ sidebar-topbar-right.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ *
+ * @package legalpress
+ */
+
+
+if ( ! is_active_sidebar( 'topbar-right' ) ) :
+	return;
+endif;
+
+?>
+<div class="right-sidebar-wrapper">
+	<?php dynamic_sidebar( 'topbar-right' ); ?>
+</div>
+
